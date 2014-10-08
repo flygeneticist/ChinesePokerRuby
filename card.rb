dd394f05819f2cb7980649f80e654f7bc028cd19
Kevin Keller <flygeneticist@gmail.com>
2014-10-08 09:43:58 -0600

    fixed hash syntax issues in card class

--- card.rb
+++ card.rb
@@ -1,8 +1,8 @@
 class Card
-    @@suits = {1: 'Clubs', 2: 'Diamonds', 3: 'Hearts', 4: 'Spades'}
-    @@values = {1: 'One', 2: 'Two', 3: 'Three', 4: 'Four', 5: 'Five', 
-                6: 'Six', 7: 'Seven', 8: 'Eight', 9: 'Nine', 10: 'Ten',
-                11: 'Jack', 12: 'Queen', 13: 'King'}
+    @@suits = {1=>'Clubs', 2=>'Diamonds', 3=>'Hearts', 4=>'Spades'}
+    @@values = {1=>'One', 2=>'Two', 3=>'Three', 4=>'Four', 5=>'Five', 
+                6=>'Six', 7=>'Seven', 8=>'Eight', 9=>'Nine', 10=>'Ten',
+                11=>'Jack', 12=>'Queen', 13=>'King'}
 
     attr_accessor :value, :suit
     
